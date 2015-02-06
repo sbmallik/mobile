776091a1736ed7b17c45d4bcac5ca2781479b1fc
sbmallik <smallik@gannett.com>
2015-02-06 10:59:10 -0500

    Updated the asset type identification logic

--- features/step_definitions/pq-two-vp-steps.rb
+++ features/step_definitions/pq-two-vp-steps.rb
@@ -2,7 +2,7 @@ When(/^Filter "(.*?)" from Media Type Overlay$/) do | media_type |
   expect(page).to have_css('.header-logo.header-media')
   find('.media-filter-link').click
   find('.media-select-option', :text => media_type).click
-  find('.overlay-close-btn').click
+  find('.overlay-close-btn.top').click
 end
 
 Then(/^I should see "(.*?)" heading$/) do | media_type |
@@ -14,5 +14,5 @@ Then(/^I should see only "(.*?)" on the page$/) do | media_type |
 end
 
 Then(/^I should see both videos and photos on the page$/) do
-  all('.media-container-details').each { | asset | asset.first('span')["class"] == ('.media-pley-icon' || '.media-gallery-icon') }
+  all('.media-container-details').each { | asset | asset.find(asset.first('a')["class"] == 'grid-photo-link' ? '.media-gallery-icon' : '.media-play-icon') }
 end
